1c53397b75e943fce8bf60cee22837710cb32a2f
petelindgren <=>
2022-05-01 13:39:14 -0400

    Issue #21 add requirements example with Cookie transport and DB strategy

---
+++ examples/requirements/08_docker_compose_fastapi_users_jwt_bearer_db_cookie/app/strategies.py
@@ -0,0 +1,31 @@
+from typing import List
+
+from app.db import get_access_token_db
+from app.models import AccessToken, UserCreate, UserDB
+from app.settings import ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM, SECRET_KEY
+from fastapi import Depends
+from fastapi_users.authentication import JWTStrategy
+from fastapi_users.authentication.strategy.db import (
+    AccessTokenDatabase,
+    DatabaseStrategy,
+)
+
+
+class Session:
+    pass
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(
+        secret=SECRET_KEY,
+        algorithm=ALGORITHM,
+        lifetime_seconds=60 * ACCESS_TOKEN_EXPIRE_MINUTES,
+    )
+
+
+def get_database_strategy(
+    access_token_db: AccessTokenDatabase[AccessToken] = Depends(get_access_token_db),
+) -> DatabaseStrategy[UserCreate, UserDB, AccessToken]:
+    return DatabaseStrategy(
+        access_token_db, lifetime_seconds=60 * ACCESS_TOKEN_EXPIRE_MINUTES
+    )
